59a511f925f18d0cdadb4505815dd98b5f0af3c0
MauroZorzin <60018820+sZeta99@users.noreply.github.com>
2022-07-07 19:29:41 +0200

    Set Up And Add Item

---
+++ src/main/java/com/noir/rpg_exp_mg/RpgExpansionMagicModuleMod.java
@@ -0,0 +1,21 @@
+package com.noir.rpg_exp_mg;
+
+import com.noir.rpg_exp_mg.ModItems.ModItems;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("rpg_exp_mg")
+public class RpgExpansionMagicModuleMod {
+
+    public static final String MOD_ID = "rpg_exp_mg";
+
+    public RpgExpansionMagicModuleMod() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+    }
+
+}
